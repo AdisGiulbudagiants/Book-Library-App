0841e90e5b620e333356e1d65cb0fb7cb0f16139
Adis Giulbudagiants <adisforwork2001@gmail.com>
2023-12-07 17:15:22 +0400

    Filter by author

--- frontend/src/redux/slices/filterSlice.js
+++ frontend/src/redux/slices/filterSlice.js
@@ -1,6 +1,6 @@
 import { createSlice } from "@reduxjs/toolkit"
 
-const initialState = { title: "" }
+const initialState = { title: "", author: "" }
 
 const filterSlice = createSlice({
   name: "filter",
@@ -10,14 +10,19 @@ const filterSlice = createSlice({
       state.title = action.payload
       // return {...state, title: action.payload}
     },
+    setAuthorFilter: (state, action) => {
+      state.author = action.payload
+    },
     resetFilters: (state) => {
       return initialState
     },
   },
 })
 
-export const { setTitleFilter, resetFilters } = filterSlice.actions
+export const { setTitleFilter, setAuthorFilter, resetFilters } =
+  filterSlice.actions
 
 export const selectTitleFilter = (state) => state.filter.title
+export const selectAuthorFilter = (state) => state.filter.author
 
 export default filterSlice.reducer
